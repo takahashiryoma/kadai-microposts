e800e3c6283e49d30bdd703f92a4e989587ea799
ryoma.takahashigit config --global user.email takahashiryouma0102@gmail.com <ubuntu@ip-172-31-30-8.ap-northeast-1.compute.internal>
2021-11-19 13:03:46 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'microposts/create'
+  get 'microposts/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
@@ -12,4 +11,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
